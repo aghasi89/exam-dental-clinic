fc27974274c68d57631ec695f97953a2af97c3b7
HarutHovsepyan <harut.hovsepyan.199419@gmail.com>
2022-11-13 18:48:05 +0400

    added icons folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Logo from "./icons/logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={Logo} className="app-logo" alt='Logi'/>
       </header>
     </div>
   );
