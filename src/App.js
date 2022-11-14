7ef87000e6999b64b1307bc96d1e8db0fe7ffbf4
MarMkrtchyan <mariam.2002@internet.ru>
2022-11-14 14:15:23 +0400

    first section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
+import "./App.css";
 import Header from "./pages/Header/Header";
 import Nav from "./pages/Nav/Nav";
+import FirstSection from "./pages/FirstSection/FirstSection";
+
 export default function App() {
   return (
     <div className="App">
-    <Header/>
-    <Nav/>
+      <Header />
+      <Nav />
+      <FirstSection />
     </div>
   );
 }
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,8 @@
+export default function Cart(props) {
+  return (
+    <div className="cart-maket">
+      <div>{props.title}</div>
+      <div>{props.text}</div>
+    </div>
+  );
+}
