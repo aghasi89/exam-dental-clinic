ae975faa127ffc3876684f53c508be11d8979293
yana-khachaturyan <yana.khachaturyan.02@bk.ru>
2022-11-16 18:11:37 +0400

    designed language bar

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -2,11 +2,15 @@ import Logo from "../../icons/logo.png";
 import Phone from "../../icons/phone.png";
 import Messege from "../../icons/messege.png";
 
+import Am from "../../icons/am.png";
+import Ru from "../../icons/ru.png";
+import En from "../../icons/en.png";
 
 export default function Header() {
     return (
       <div className="header">
         <img src={Logo} className="logo" alt='Logo'/>
+  
         <div className="header-icons-info">
         <div className="phone-div">
         <img src={Phone} className="phone-icon" alt='Phone'/>
@@ -16,6 +20,13 @@ export default function Header() {
         <img src={Messege} className="messege-icon" alt='Messege'/>
         <span>loremIpsum@gmail.com</span>
         </div>
+        
+        </div>
+        <div className="languages-part">
+        <img src={Ru} className="ru" alt='Ru'/>
+        <img src={Am} className="am" alt='Am'/>
+        <img src={En} className="en" alt='En'/>
+   
         </div>
       </div>
     );
